22487f85dde4fe434dfa39d72a7bbcded9281676
Matheus Calmon <sanderhus15@outlook.com>
2024-12-18 16:54:11 -0300

    adicionando exercicios estrutura sequencial

---
+++ python_brasil_practice/estrutura_sequencial/ex16.py
@@ -0,0 +1,7 @@
+#calcule os descontos e o salário líquido, conforme a tabela abaixo:
+#+ Salário Bruto : R$
+#- IR (11%) : R$
+#- INSS (8%) : R$
+#- Sindicato ( 5%) : R$
+#= Salário Liquido : R$
+#Obs.: Salário Bruto - Descontos = Salário Líquido.
---
+++ python_brasil_practice/estrutura_sequencial/ex19.py
@@ -0,0 +1,5 @@
+#Informe ao usuário as quantidades de tinta a serem compradas e os respectivos preços em 3 situações:
+#comprar apenas latas de 18 litros;
+#omprar apenas galões de 3,6 litros;
+#misturar latas e galões, de forma que o desperdício de tinta seja menor. 
+# Acrescente 10% de folga e sempre arredonde os valores para cima, isto é, considere latas cheias.
