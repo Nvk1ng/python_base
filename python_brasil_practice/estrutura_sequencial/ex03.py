2cb177b7dcc9097d9eac65a90c90bb9f134149b2
matheus sanderhus <sanderhus15@outlook.com>
2024-12-22 19:58:34 -0300

    realizado os primeiros exercicios

--- python_brasil_practice/estrutura_sequencial/ex03.py
+++ python_brasil_practice/estrutura_sequencial/ex03.py
@@ -1,1 +1,7 @@
 # Faça um Programa que peça dois números e imprima a soma.
+
+numero1 = int(input("Digite o primeiro numero:"))
+numero2 = int(input("Digite o segundo numero:"))
+soma = numero1 + numero2
+
+print(f"A soma de {numero1} + {numero2} = {soma}")
