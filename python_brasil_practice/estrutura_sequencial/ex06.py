d3afdffa71426f86a7f2737cb55622b21aaee2a4
matheus sanderhus <sanderhus15@outlook.com>
2024-12-30 20:31:32 -0300

    realizado  exercicios

--- python_brasil_practice/estrutura_sequencial/ex06.py
+++ python_brasil_practice/estrutura_sequencial/ex06.py
@@ -1,1 +1,9 @@
 # Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.
+
+raio = int(input("Digite o valor do raio: "))
+
+pi = 3.14
+
+area = pi * (raio * raio)
+
+print(f"A area do circulo é igual a: {area}")
--- python_brasil_practice/estrutura_sequencial/ex07.py
+++ python_brasil_practice/estrutura_sequencial/ex07.py
@@ -1,1 +1,10 @@
 # Faça um Programa que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário.
+
+valor1 = int(input("Digite o valor do quadrado: "))
+
+area = valor1 * valor1
+
+dobro = valor1 * 2 
+
+
+print(f"O valor da areado do quadrado e igual a {area} e o seu dobro e igual a {dobro}")
