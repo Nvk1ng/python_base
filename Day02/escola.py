a5e4bb0b1d270ce4e30e0e4a5e67ffa60d22aa45
Matheus Calmon <sanderhus15@outlook.com>
2025-04-16 13:39:23 -0300

    atividades com listas e tuplas

--- Day02/escola.py
+++ Day02/escola.py
@@ -15,3 +15,27 @@ aula_ingles = ["Erik", "Maia", "Joana", "Carlos", "Antonio"]
 aula_musica = ["Erik", "Carlos", "Maria"]
 aula_danca = ["Gustavo", "Sofia", "Joana", "Antonio"]
 
+atividades = [("Ingles", aula_ingles), ("Musica", aula_musica), ("Dança", aula_danca)]
+
+for nome_atividade, atividade in atividades:
+
+    print(f"Alunos da atividade {nome_atividade}\n")
+    
+    atividade_sala1 = []
+    atividade_sala2 = []
+
+    for aluno in atividade:
+        if aluno in sala1:
+            atividade_sala1.append(aluno)
+        elif aluno in sala2:
+            atividade_sala2.append(aluno)
+
+
+    print("sala1", atividade_sala1)
+    print("Sala2", atividade_sala2)
+
+    print()
+    print("#" * 50)
+
+
+
